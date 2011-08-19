0624625739b10708c49767c5ab4062fe8904263e
Malte S. Stretz <mss@apache.org>
2011-08-19 17:23:48 +0200

    Add gzip example.

--- manifest-test.php
+++ manifest-test.php
@@ -25,18 +25,25 @@ $c = $conn->create_container(
 );
 $c->make_public();
 
+$gzip = $conf['test']['gzip'];
+$data = "foo\n";
+if ($gzip)
+    $data = gzip($data);
+
 $o = $c->create_object($conf['test']['data']);
 $o->headers  = array(
     'Content-Disposition' => 'inline',
+    'Content-Encoding'    => $gzip ? "gzip" : "identity",
 );
 $o->metadata = array(
     'Data' => 'test',
 );
-$o->write("foo\n", 4);
+$o->write($data, strlen($data));
 
 $o = $c->create_object($conf['test']['link']);
 $o->headers  = array(
     'Content-Disposition' => 'attachment',
+    'Content-Encoding'    => $gzip ? "gzip" : "identity",
 );
 $o->metadata = array(
     'Link' => 'test',
@@ -52,4 +59,32 @@ echo("\n-------------------------\n\n\n" .
      $o->public_uri() .
     "\n\n\n-------------------------\n"
 );
+exit;
 
+function gzip($data) {
+    $pipe = array();
+    $gzip = proc_open(implode(" ", array(
+                "gzip",
+                "--best",
+                "--to-stdout",
+                "--force",
+                "--no-name",
+                "--quiet"
+            )), array(
+                array("pipe", "r"),
+                array("pipe", "w")
+            ), $pipe
+        );
+    if ($gzip === false) {
+        header("Status: 503");
+        header("X-Error: popen");
+        echo("gzip error\n");
+        exit(1);
+    }
+    fwrite($pipe[0], $data);
+    fclose($pipe[0]);
+    $data = stream_get_contents($pipe[1]);
+    fclose($pipe[1]);
+    proc_close($gzip);
+    return $data;
+}
